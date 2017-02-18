01dadf73cb8aa632ee81108d25615213fdf196ff
vysakhpr <emailofvysakh@gmail.com>
2017-02-18 22:39:41 +0530

    Populated Use and Def Sets

--- LivenessAnalysis.cpp
+++ LivenessAnalysis.cpp
@@ -5,18 +5,28 @@
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Use.h"
+#include <set>
 
 using namespace llvm;
 
 namespace 
 {
+
+	struct Bblocks
+	{
+		std::set<StringRef> use_set, def_set;
+	};
+
   	struct Live : public FunctionPass 
   	{
     	static char ID;
     	Live() : FunctionPass(ID) {}
+    	Bblocks* BB=NULL;
    		bool runOnFunction(Function &F) override 
    		{
-	   		for (Function::iterator block_iterator = F.begin(), block_iterator_end = F.end(); block_iterator != block_iterator_end; ++block_iterator)
+   			BB=new Bblocks[F.size()];
+   			int count=0;
+	   		for (Function::iterator block_iterator = F.begin(), block_iterator_end = F.end(); block_iterator != block_iterator_end; ++block_iterator,count++)
    			{
    				//errs() << "Basic block_iterator (name=" << block_iterator->getName() << ") has "<< block_iterator->size() << " instructions.\n------------------------------------------------\n\n";
    				BasicBlock& block= *block_iterator;				
@@ -24,19 +34,35 @@ namespace
              	{
              		errs() << *i << "\n";
              		Instruction *pi=&*i;
-             		errs() << "\t"<< pi->getName();
+             		if(pi->getName().compare(""))
+	             		BB[count].def_set.insert(pi->getName());
              		for (Use &U : pi->operands()) 
              		{
   						Value *v = U.get();
   						if(isa<Instruction>(v))
-  							errs() << "\t"<< v->getName();
+  						{
+  							BB[count].use_set.insert(v->getName());
+  						}
 					}
-					errs()<<"\n";
-             		//errs()<<"\t\t\t\t\t\t"<<vi->getName()<<"\t"<<(pi->getOperand(0))->getName()<<"\n";
-
              	}
-
    			}
+
+   			/*std::set<StringRef>::iterator it;
+   			for (int j = 0; j < count; ++j)
+   			{
+   				errs() << "Use Set of block" <<j <<"\n------------\n";
+   				for (it = BB[j].use_set.begin(); it != BB[j].use_set.end(); ++it)
+				{
+    				errs() <<*it <<"\n";
+				}
+
+				errs() <<"Def Set of block" <<j <<"\n------------\n";
+   				for (it=BB[j].def_set.begin(); it != BB[j].def_set.end(); ++it)
+				{
+    				errs() <<*it <<"\n";
+				}	
+   			}*/
+			
    			return false;
    		}
 	};
